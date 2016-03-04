4a9422cd73f0f9cb8873df03948ae1271f9de906
Alexandre D'Eschambeault <adeschambeault@exolnet.com>
2016-03-04 16:08:04 -0500

    Ajout du getter/setter pour le country

--- src/Country.php
+++ src/Country.php
@@ -75,4 +75,23 @@ class Country extends Model {
 
 		return $this;
 	}
+
+	/**
+	 * @return string
+	 */
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	/**
+	 * @param string $name
+	 * @return $this
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+
+		return $this;
+	}
 }
